4c9eb4b38efc429fdcaf249ab3565f75add54efe
tumillanino <anthony.tumia@live.com.au>
2026-03-26 16:39:13 +1100

    some more branding

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -5,3 +5,12 @@ mv /tmp/files/assets/images/distributor-logo.svg /usr/share/icons/hicolor/scalab
 mv /tmp/files/assets/splash/default.jxl /usr/share/backgrounds/default.jxl
 mv /tmp/files/assets/splash/default-dark.jxl /usr/share/backgrounds/default-dark.jxl
 mv /tmp/files/assets/images/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
+mv /tmp/files/assets/images/fedora-gdm-logo.png /usr/share/pixmaps/fedora-gdm-logo.png
+mv /tmp/files/assets/images/fedora-logo-small.png /usr/share/pixmaps/fedora-logo-small.png
+mv /tmp/files/assets/images/fedora-logo-sprite.png /usr/share/pixmaps/fedora-logo-sprite.png
+mv /tmp/files/assets/images/fedora-logo-sprite.svg /usr/share/pixmaps/fedora-logo-sprite.svg
+mv /tmp/files/assets/images/fedora-logo.png /usr/share/pixmaps/fedora-logo.png
+mv /tmp/files/assets/images/fedora_logo_med.png /usr/share/pixmaps/fedora_logo_med.png
+mv /tmp/files/assets/images/fedora_whitelogo.svg /usr/share/pixmaps/fedora_whitelogo.svg
+mv /tmp/files/assets/images/fedora_whitelogo_med.png /usr/share/pixmaps/fedora_whitelogo_med.png
+mv /tmp/files/assets/images/system-logo-white.png /usr/share/pixmaps/system-logo-white.png
