51f18558fc58e7522e08b9a08231d0b87e22998c
tumillanino <anthony.tumia@live.com.au>
2026-04-08 07:08:10 +1000

    remove oh-my-posh as it has annoying quirks

--- files/scripts/installdecentsampler.sh
+++ files/scripts/installdecentsampler.sh
@@ -7,6 +7,6 @@ tar xvf /tmp/decent-sampler.tar.gz -C /tmp
 cd /tmp/Decent_Sampler-1.18.1-Linux-Static-x86_64
 ./install.sh
 
-rm -rf /tmp/decent-sampler.tar.gz /tmp/Renoise* || true
+rm -rf /tmp/decent-sampler.tar.gz /tmp/Decent* || true
 
 exit 0
